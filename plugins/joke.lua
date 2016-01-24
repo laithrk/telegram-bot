fd1f5bafddb947461c5ea62424fb38b58e988527
erfan <erfan.hellboy79@gmail.com>
2016-01-24 20:31:29 +0330

    Create joke.lua

---
+++ plugins/joke.lua
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def function(msg, matches, peer):
+  req = requests.get('http://tambal.azurewebsites.net/joke/random')
+  html = req.text
+  jdata = json.loads(html)
+  return jdata["joke"]
+
+plugin = {
+  'name': "Joke",
+  'tag': "joke",
+  'patterns': ["^/joke$"],
+  'function': function,
+  'elevated': False, 
+  'usage' : "/joke",
+  'desc' : "Tells a joke"
+  }
